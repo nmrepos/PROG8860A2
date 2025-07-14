1e36de8828be0c9c6874a5318f67aa7b59271470
Nidhun <hello@example.com>
2025-07-13 21:57:35 -0400

    Initial commit for CDK assignment

--- lib/a2-stack.ts
+++ lib/a2-stack.ts
@@ -1,16 +1,36 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class A2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const myBucket = new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'A2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new lambda.Function(this, 'MyLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          console.log("Lambda invoked");
+          return { statusCode: 200, body: "Hello, World!" };
+        };
+      `),
+      environment: {
+        BUCKET_NAME: myBucket.bucketName,
+      },
+    });
+
+    new dynamodb.Table(this, 'MyTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableName: 'MyTable',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
